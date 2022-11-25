6816272acef6c8809cfd59b9e9e00ca5149a070c
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-11-25 13:45:57 -0600

    Updated Icons

--- index.js
+++ index.js
@@ -27,15 +27,23 @@ function displayTemperature(repsonce) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(repsonce.data.main.temp);
   cityElement.innerHTML = repsonce.data.name;
   descriptionElement.innerHTML = repsonce.data.weather[0].description;
   humidityElement.innerHTML = repsonce.data.main.humidity;
   windSpeedElement.innerHTML = Math.round(repsonce.data.wind.speed);
   dateElement.innerHTML = formatDate(repsonce.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${repsonce.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", repsonce.data.weather[0].description);
 }
 
 let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "los angeles";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);

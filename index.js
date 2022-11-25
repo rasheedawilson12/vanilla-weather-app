21bd72c345220d6293b5cf6cccdf7f860d7f64ad
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-11-25 12:28:01 -0600

    Updated Current Temperature

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function displayTemperature(repsonce) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = repsonce.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  temperatureElement.innerHTML = Math.round(repsonce.data.main.temp);
+  cityElement.innerHTML = repsonce.data.name;
+  descriptionElement.innerHTML = repsonce.data.weather[0].description;
+  humidityElement.innerHTML = repsonce.data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(repsonce.data.wind.speed);
 }
-let apiKey = "9t257307ca476845c0efc0e5f24o9bc3";
+
+let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);

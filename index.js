1f92bf76ce88ddad19085e1424209acebc22b408
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-11-25 13:28:17 -0600

    Created date and time

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0:${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(repsonce) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(repsonce.data.main.temp);
   cityElement.innerHTML = repsonce.data.name;
   descriptionElement.innerHTML = repsonce.data.weather[0].description;
   humidityElement.innerHTML = repsonce.data.main.humidity;
   windSpeedElement.innerHTML = Math.round(repsonce.data.wind.speed);
+  dateElement.innerHTML = formatDate(repsonce.data.dt * 1000);
 }
 
 let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";

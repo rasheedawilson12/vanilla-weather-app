81b69d630e2e3c3f5a642c1b0e2fcd079f09d661
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-11-26 10:19:13 -0600

    Weekly forecast template in Javascript

--- index.js
+++ index.js
@@ -20,6 +20,36 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `         <div class="col-2">
+                <div class="weather-forecast-date">
+                ${day}
+                </div>
+                <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" width="42"/>
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max"> 
+                    18°
+                  </span>
+                  <span class="weather-forecast-temperature-min">
+                    12°
+                  </span>
+                </div>
+                
+              </div>
+            `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(repsonce) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +116,5 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("New York");
+
+displayForecast();
